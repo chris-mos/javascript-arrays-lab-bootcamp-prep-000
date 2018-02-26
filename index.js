3dd3bf2cc337e414fab4e12bfa30cbb69ab5a084
Christopher Moser <moser121@icloud.com>
2018-02-26 21:39:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name){
+  kittens.push(name)
+  return kittens
+}
+function destuctivelyPrependKitten(name){
+  kittens.unshift(name)
+  return kittens
+}
+function destructivelyRemoveLastKitten(name){
+  kittens.shift(name)
+  return kittens
+}
+function destructivelyRemoveFirtKitten(name){
+  kittens.pop(name)
+  return kittens
+}
+function appendKitten(name){
+  [kittens, ...name]
+  return kittens
+}
+function prependKitten(name){
+ [name..., kittens]
+ return kittens
+ 
+}
